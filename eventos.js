530aa148edb0ecfa5b17681ec9e56eda4975ce93
GlauciaAndrade <87622024+GlauciaAndrade@users.noreply.github.com>
2022-02-03 10:54:45 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,27 @@
+let idadepart = 18 
+if (idadepart >= 18 ) {
+    console.log(" Idade permitida,  cadastrar")
+}else {
+    console.log("Menor que 18  não é permitido cadastrar" )
+}
+let dataEvento = 05/02/2022
+let dataHj = 03/02/2022
+if (dataEvento >= dataHj) {
+    console.log(" Data permitida, cadastrar")
+}else {
+    console.log("Não é permitido cadastrar")
+}
+let listaPart = ["Ana", "Bia", "Joao", "Jose", "Gil"]
+
+for(let contador = 0; contador < listaPart.length; contador++) {
+    console.log(listaPart[contador])
+}
+let quantidadePartic = 8
+if (quantidadePartic <= 8) {
+    console.log("Quantidade participante aceito, cadastrar")
+}else {
+    console.log("Quantidade participante excedida,não cadastrar")
+}
+    
+
+     
